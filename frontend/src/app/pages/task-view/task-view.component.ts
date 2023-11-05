8ed46f6822c6e3db4acdd9115846709acc8e62b5
kuzzmicz <kuzzmiczpies@gmail.com>
2023-11-05 04:02:12 +0100

    Connected API to frontend.

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
 
 @Component({
   selector: 'app-task-view',
   templateUrl: './task-view.component.html',
   styleUrls: ['./task-view.component.scss']
 })
-export class TaskViewComponent {
+export class TaskViewComponent{
 
+  lists: any;
+  tasks: any;
+  constructor(private taskService: TaskService, private route: ActivatedRoute){}
+     ngOnInit(){}
+     createNewList(){
+this.taskService.createList('TEST').subscribe((response: any)=>{
+    console.log(response);
+});
+     }
+ 
 }
