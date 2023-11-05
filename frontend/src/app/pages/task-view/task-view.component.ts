54494c6943169bcbe0379ccec940c7ed3bc3a31a
kuzzmicz <kuzzmiczpies@gmail.com>
2023-11-05 04:52:38 +0100

    Task are dispayed from database.

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -10,12 +10,18 @@ import { TaskService } from 'src/app/task.service';
 export class TaskViewComponent{
 
   lists: any;
+  tasks: any;
   constructor(private taskService: TaskService, private route: ActivatedRoute){}
      ngOnInit(){
       this.route.params.subscribe(
         (params: Params) => {
         console.log(params);
+        this.taskService.getTasks(params['listId']).subscribe((tasks: any)=>{
+          this.tasks = tasks; 
+        })
         }
+
+       
       )
  this.taskService.getLists().subscribe((lists: any)=>{
    this.lists=lists;
